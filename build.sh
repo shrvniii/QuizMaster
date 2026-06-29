096a632678910653b763ef24a34cb9e0244c797b
Shravani Sawant <shravanisantosh0719@gmail.com>
2026-06-29 14:10:43 +0530

    Automate superuser creation in build.sh

--- build.sh
+++ build.sh
@@ -10,3 +10,6 @@ python manage.py collectstatic --no-input
 
 # Run database migrations
 python manage.py migrate
+
+# Automatically create the superuser if it doesn't exist
+python -c "import django; django.setup(); from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin123') if not User.objects.filter(username='admin').exists() else None"
